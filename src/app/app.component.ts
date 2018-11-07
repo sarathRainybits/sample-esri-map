12e4a07816f8e7c0e95940bfd04a1b4121004e2b
Sarath PS <sarath@rainybits.com>
2018-11-07 14:18:07 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'srt-esri-sample';
+  // Set our map properties
+  mapCenter = [-122.4194, 37.7749];
+  basemapType = 'satellite';
+  mapZoomLevel = 12;
+
+  // See app.component.html
+  mapLoadedEvent(status: boolean) {
+    console.log('The map loaded: ' + status);
+  }
 }
