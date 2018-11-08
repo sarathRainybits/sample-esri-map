a78f16cdcf2d2ec38dd7a8ae5f9e0513a6bb205b
Sarath PS <sarath@rainybits.com>
2018-11-08 13:20:47 +0530

    esri tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   // Set our map properties
+  extentArray = ["-13215046.250","4017332.161","-13147657.669","4076317.766"];
   mapCenter = [-122.4194, 37.7749];
-  basemapType = 'satellite';
-  mapZoomLevel = 12;
-
+  basemapType = 'topo';
+  mapZoomLevel = 10;
+spatialReference = 3857;
+gisLayer = "https://ags_infra2.sedaru.com/arcgis/rest/services/ladwp/ladwp_gis/MapServer/";
+pipeLayer = "https://ags_infra2.sedaru.com/arcgis/rest/services/ladwp/ladwp_gis/MapServer/11";
   // See app.component.html
   mapLoadedEvent(status: boolean) {
     console.log('The map loaded: ' + status);
--- src/app/esri-map/esri-map.component.ts
+++ src/app/esri-map/esri-map.component.ts
@@ -6,7 +6,9 @@ import esri = __esri;
   templateUrl: './esri-map.component.html',
   styleUrls: ['./esri-map.component.css']
 })
-export class EsriMapComponent implements OnInit {  @Output() mapLoaded = new EventEmitter<boolean>();
+export class EsriMapComponent implements OnInit {
+
+  @Output() mapLoaded = new EventEmitter<boolean>();
   @ViewChild('mapViewNode') private mapViewEl: ElementRef;
 
   /**
@@ -15,8 +17,30 @@ export class EsriMapComponent implements OnInit {  @Output() mapLoaded = new Eve
    * @private _basemap sets type of map
    */
   private _zoom: number = 10;
+  private _gisLayer: string = "";
+  private _pipeLayer: string = "";
   private _center: Array<number> = [0.1278, 51.5074];
   private _basemap: string = 'streets';
+  private _extentArray: string[] = [];
+  private _spatialReference: number = 3587;
+
+  @Input()
+  set extentArray(extentArray: string[]) {
+    this._extentArray = extentArray;
+  }
+
+  get extentArray(): string[] {
+    return this._extentArray;
+  }
+
+  @Input()
+  set spatialReference(zoom: number) {
+    this._spatialReference = zoom;
+  }
+
+  get spatialReference(): number {
+    return this._spatialReference;
+  }
 
   @Input()
   set zoom(zoom: number) {
@@ -25,6 +49,24 @@ export class EsriMapComponent implements OnInit {  @Output() mapLoaded = new Eve
 
   get zoom(): number {
     return this._zoom;
+  }
+
+  @Input()
+  set pipeLayer(layer: string) {
+    this._pipeLayer = layer;
+  }
+
+  get pipeLayer(): string {
+    return this._pipeLayer;
+  }
+
+  @Input()
+  set gisLayer(layer: string) {
+    this._gisLayer = layer;
+  }
+
+  get gisLayer(): string {
+    return this._gisLayer;
   }
 
   @Input()
@@ -49,37 +91,91 @@ export class EsriMapComponent implements OnInit {  @Output() mapLoaded = new Eve
 
   async initializeMap() {
     try {
-      const [EsriMap, EsriMapView] = await loadModules([
+      const [EsriMap, EsriMapView, TileLayer, Extent, SpatialReference, FeatureLayer, IdentifyTask, IdentifyParameters] = await loadModules([
         'esri/Map',
-        'esri/views/MapView'
+        'esri/views/MapView',
+        'esri/layers/TileLayer',
+        'esri/geometry/Extent',
+        'esri/geometry/SpatialReference',
+        'esri/layers/FeatureLayer',
+        'esri/tasks/IdentifyTask',
+        'esri/tasks/support/IdentifyParameters'
       ]);
 
+      const customExtentAndSR = Extent(parseFloat(this._extentArray[0]), parseFloat(this._extentArray[1]), parseFloat(this._extentArray[2]), parseFloat(this._extentArray[3]), SpatialReference({ "wkid": this._spatialReference }));
+
+      var popupTemplate = { // autocasts as new PopupTemplate()
+        title: "Outage Event : {description}",
+        content: "customers affected : <b>{n_customers}</b>"
+      };
+      // Typical usage
+      const gisLayer = new FeatureLayer({
+        url: this._gisLayer,
+        outFields: ["*"],
+        popupTemplate: popupTemplate
+
+      });
+
+      var pipeLayer = new FeatureLayer({
+        url: this._pipeLayer,
+      });
       // Set type of map
       const mapProperties: esri.MapProperties = {
-        basemap: this._basemap
+        basemap: this._basemap,
+        layers: [pipeLayer, gisLayer]
       };
 
       const map: esri.Map = new EsriMap(mapProperties);
 
+
       // Set type of map view
       const mapViewProperties: esri.MapViewProperties = {
         container: this.mapViewEl.nativeElement,
-        center: this._center,
+        extent: customExtentAndSR,
         zoom: this._zoom,
         map: map
       };
 
       const mapView: esri.MapView = new EsriMapView(mapViewProperties);
+      var identifyTask: any = {};
+      var params: any = {};
 
-      // All resources in the MapView and the map have loaded.
-      // Now execute additional processes
+      let executeIdentifyTask = function (event) {
+        // Set the geometry to the location of the view click
+        params.geometry = event.mapPoint;
+        params.mapExtent = mapView.extent;
+
+
+        // This function returns a promise that resolves to an array of features
+        // A custom popupTemplate is set for each feature based on the layer it
+        // originates from
+        identifyTask.execute(params).then(function (response) {
+
+          var results = response.results;
+          return results.map(function (result) {
+            return response.results[0].feature;
+
+          });
+        }); // Send the array of features to showPopup()
+      }
+
       mapView.when(() => {
+        // executeIdentifyTask() is called each time the view is clicked
+        mapView.on("click", executeIdentifyTask);
+        // Create identify task for the specified map service
+        identifyTask = new IdentifyTask(this._gisLayer);
+        // Set the parameters for the Identify
+        params = new IdentifyParameters();
+        params.tolerance = 10;
+        params.layerIds = [0];
+        params.layerOption = "all";
+        params.width = mapView.width;
+        params.height = mapView.height;
         this.mapLoaded.emit(true);
       });
     } catch (error) {
       console.log('We have an error: ' + error);
     }
-
   }
 
   ngOnInit() {
